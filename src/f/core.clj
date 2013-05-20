020aa728a177bbcdb274bc546c5427c8233e3ca7
pimgeek <walteriii@gmail.com>
2013-05-20 12:06:23 +0800

    move clj file under src dir.

--- src/f/core.clj
+++ src/f/core.clj
@@ -3,9 +3,21 @@
 
 ; need a function to clear memory
 ; prepare for gui env
-(load-file "pre.clj")
-
+(use 'clojure.repl)
+(use 'seesaw.core)
+(use 'seesaw.dev)
+(native!)
+(defn p [frame ui] 
+  (config! frame :content ui
+    )
+  )
+(defn v [frame]
+  (-> frame pack! show!
+    )
+  )
+  
 ; define main frame and its widgets
+
 (def main-frm (frame :title "Flow"))
 (def trigger-btn (button :text "Open"))
 (p main-frm trigger-btn)
