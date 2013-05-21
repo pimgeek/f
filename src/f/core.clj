54bf2b72eae88e66ad4d0e0d27e2c4a2ddf200c5
pimgeek <walteriii@gmail.com>
2013-05-21 19:02:26 +0800

    try to add panel widget to frame

--- src/f/core.clj
+++ src/f/core.clj
@@ -1,26 +1,50 @@
 ; pimflow main
 ; by pimgeek
-
-(ns f.core ; with trptcolin's help
+; 
+; setup namespace
+(ns f.core           ; with trptcolin's help and his blog post.
   (:use
     [clojure.repl]
     [seesaw.core]
+    [seesaw.mig]
     [seesaw.dev]
-	)
   )
-
-(defn p [frame ui] 
+)
+;
+; setup util functions 
+(defn p [frame ui]    ; put ui on frame
   (config! frame :content ui
-    )
   )
-(defn v [frame]
+)
+(defn v [frame]       ; make the frame visible
   (-> frame pack! show!
-    )
   )
+)
   
 ; define main frame and its widgets
 
-(def main-frm (frame :title "Flow"))
-(def trigger-btn (button :text "Open"))
-(p main-frm trigger-btn)
-(v main-frm)
+(defn run-app []
+  (def main-frm (frame :title "Flow"))
+  (def trigger-btn (button :text "Open"))
+  (def mpanel
+    (mig-panel
+      :constraints ["wrap 2"
+      ]
+      :items [
+        [trigger-btn "wrap"]
+      ]
+    )
+  )
+  (add! mpanel ["2nd" "wrap"])
+  (add! mpanel ["3rd"])
+  (def bpanel (border-panel
+    :north trigger-btn
+    :center (label :text "center")
+    :west (label :text "west")
+    :east (label :text "east")
+    :south (label :text "source")
+    :border [5 "dashed" 10]
+    :vgap 50 :hgap 50 :border 15))
+  (p main-frm bpanel)
+  (v main-frm)
+)
