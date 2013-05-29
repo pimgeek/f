8060131a1db9bea52dd87b688fce637c88f145dd
pimgeek <walteriii@gmail.com>
2013-05-30 00:08:23 +0800

    add new code file for scheme playing

---
+++ src/f/guile.scm
@@ -0,0 +1,13 @@
+#!/usr/bin/guile
+!#
+
+(define atom?
+ (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+(define lat?
+  (lambda (l)
+    (cond
+      ((null? l) #t)
+      ((atom? (car l)) (lat? (cdr l)))
+      (else #f))))
