f8727d0f4b98b176951aa7a624ab99242f1609b4
pimgeek <walteriii@gmail.com>
2013-06-27 23:11:20 +0800

    增加一个函数 - 遍历某个路径下的所有文件, 并把这些文件的绝对路径以字符串的形式依次打印出来.

---
+++ src/f/list-notes.clj
@@ -0,0 +1,30 @@
+#!/usr/bin/clojure
+
+; pimflow listing notes
+; by pimgeek
+; 
+; setup namespace
+(ns f.core           ; with trptcolin's help and his blog post.
+  (:use
+   [clojure.repl]
+   [clojure.java.io]
+  )
+)
+
+; get the list of note files by path
+; need to have edge condition verification
+(defn list-notes [path]
+  (def fnames
+    (for 
+      [x (file-seq (file path))]
+      (.getPath x)
+    )
+  )
+  (doseq [x fnames]
+    (prn x)
+  )
+)
+
+; test running
+(list-notes "/mnt/o/_zim/.")
+
