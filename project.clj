246a700351ff4397882db6542898a474b3b8c3e2
pimgeek <walteriii@gmail.com>
2013-07-04 20:48:08 +0800

    1. add tools.cli, for cmdline args parsing. 2. add lein-exec plugin, for running a single clj script with leiningen env settings.

--- project.clj
+++ project.clj
@@ -2,6 +2,12 @@
   :description "笔记流"
   :url "tbd"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"] [seesaw "1.4.3"]]
-  :main f.core)
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+           }
+  :dependencies [[org.clojure/clojure "1.5.1"] 
+                 [seesaw "1.4.3"] 
+                 [org.clojure/tools.cli "0.2.2"]
+                ]
+  :plugins [[lein-exec "0.3.0"]]
+  :main f.core
+)
