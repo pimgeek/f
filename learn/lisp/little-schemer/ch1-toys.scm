99296aa9331cf0a5ed27432868e9207c6d517b85
pimgeek <wudibiz@163.com>
2015-05-08 03:13:50 +0800

    增加更多代码片段

--- learn/lisp/little-schemer/ch1-toys.scm
+++ learn/lisp/little-schemer/ch1-toys.scm
@@ -1,3 +1,5 @@
+#!/usr/bin/racket
+
 # atom
 
 'atom
@@ -17,4 +19,55 @@
 '(atom turkey or)
 
 '(atom turkey) or
+
+'((atom turkey) or)
+
+# s-exp
+
+'xyz
+
+'(x y z)
+
+'((x y) z)
+
+'(how are you doing so far)
+
+'(((how) are) ((you) (doing so)) far)
+
+'()
+
+'(() () () ())
+
+# car of list 
+
+(car '(a b c))
+
+(car '(((hotdogs)) (and) (pickle) relish))
+
+(car (car '(((hotdogs)) (and))))
+
+# cdr of list
+
+(cdr '(a b c))
+
+(cdr '((a b c) x y z))
+
+(cdr '(hamburger))
+
+(cdr '((x) t r))
+
+(cdr 'hotdogs)
+
+(cdr '())
+
+# car and cdr
+
+(car (cdr '((b) (x y) ((c)))))
+
+(cdr (cdr '((b) (x y) ((c)))))
+
+(cdr (car (a (b (c)) d)))
+
+
+
 
