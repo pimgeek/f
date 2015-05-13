89e33c79876bd3df9075fc25a47921be6bd74b85
pimgeek <wudibiz@163.com>
2015-05-14 01:26:33 +0800

    创建一个最简单的 racket web app

---
+++ hack/pim/pim-webui.rkt
@@ -0,0 +1,55 @@
+#!/usr/bin/racket
+; 主要实现 web 访问界面
+
+#lang racket
+
+; 引入 web-server 相关模块
+(require web-server/servlet
+         web-server/servlet-env)
+
+; 设定正常访问首页时返回的内容
+(define (main-page)
+  (response/xexpr
+    `(html
+       (body
+         (h1 "Hello, World!")
+         ))))
+
+; 设定非正常访问时返回的错误信息
+(define (error-page)
+  (response/xexpr
+    `(html
+       (body
+         (h1 "Error!")
+         ))))
+
+; 设定 servlet 请求处理入口函数
+(define (pim-app req)
+  (let*
+    ([url     (request-uri req)]
+     [uri-str (url->string url)])
+    (cond
+      [(regexp-match "^/$" uri-str) (main-page)] 
+      [else (error-page)]
+      )))
+
+; 启动 web-server 并开始服务
+
+(serve/servlet
+  pim-app             ; 使用 pim-app 作为响应 servlet 请求的入口
+
+  #:port          80  ; 规定 web-server 使用 80 端口
+
+  #:servlet-path  "/" ; 规定 web-server 启动时默认打开的首页
+
+  ; 规定 servlet 负责处理路径中出现数字，字母或'/'的请求
+  ; 一旦路径出现'.'就不再由 servlet 处理了。
+  #:servlet-regexp #rx"^/[a-zA-Z0-9/-?=]*$"
+
+  #:listen-ip     #f  ; 开放局域网访问 ip
+
+  #:command-line? #t  ; 禁止自动打开浏览器
+
+  #:quit?         #t  ; 允许从浏览器端关停 web-server
+
+  )
